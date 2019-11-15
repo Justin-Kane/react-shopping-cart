1efdb39ee07c6af858ea36e191aa6efc6e1b385b
Justin Kane <JustinKane197@gmail.com>
2019-11-14 20:14:59 -0500

    edited shopping cart and products, reached MVP

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import {ProductContext} from './contexts/ProductContext'
+import { CartContext } from './contexts/CartContext'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -17,26 +19,19 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+		<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={cart}>
+				<div className="App">
+					<Navigation cart={cart} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
+	
 	);
 }
 
